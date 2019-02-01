b7a4f3b141e8448a55f27a5f61aefa29d445ddcf
davidherrera2 <46963875+davidherrera2@users.noreply.github.com>
2019-01-31 22:17:14 -0500

    Add files via upload
    Código en R del método por Bisección para calcular raíces

---
+++ Tarea#1/CodigoBiseccion.R
@@ -0,0 +1,47 @@
+
+
+Func = function(x) {x*exp(x) - pi}
+
+
+
+fBiseccion <- function(x1,x2) {
+  if(Func(x1)*Func(x2) < 0)
+  {
+  seq = seq(x1,x2,0.5)
+  
+  plot(seq,Func(seq),type="l",col="red")
+
+  ptoM = (x1+x2)/2
+  
+  error = abs(x1-x2)/2
+  
+  i = 0
+  
+  aux = Func(ptoM)
+  
+  points(rbind(c(ptoM,aux)),pch=15,cex=0.4,col="blue")
+  
+  while (error > 1.e-8) {
+    
+    i = i+1
+
+    if (Func(ptoM) == 0) break
+    
+    if (Func(ptoM)*Func(x1) < 0) x2 = ptoM else {x1 = ptoM}
+    
+    ptoM = (x1+x2)/2
+    
+    error = abs(x1-x2)/2
+    
+    aux = Func(ptoM)
+    
+    points(rbind(c(ptoM,aux)),pch=15,cex=0.4,col="blue")
+    
+    cat("Iteracion=",i,"\tFunc(x)=",Func(ptoM),"\tX=",ptoM,"\tError=",error,"\n")
+    
+  }
+  }
+  
+}
+
+fBiseccion(0,2)
