96fb963d74aab406cd2f1799ecb0afea76e5837d
davidherrera2 <46963875+davidherrera2@users.noreply.github.com>
2019-02-06 07:41:47 -0500

    Update ptofijo.R

--- Tareas/Tarea#2/ptofijo.R
+++ Tareas/Tarea#2/ptofijo.R
@@ -8,20 +8,21 @@ fPuntoFijo = function(a,b)
 {
   x = seq(a,b,0.1)
   FuncDerG = Deriv(FuncG)
-  print (FuncDerG(a))
   i = 0
-  if(FuncDerG(a) < 1)
+  if(abs(FuncDerG(a)) < 1 && ((FuncG(a)-a)*(FuncG(b)-b)) < 0)
   {
     fijo = FuncG(a)
     
     plot(x,Func(x),type = 'l', col = c("blue"))
     
     abline(h = 0)
-    
+
     aux = Func(fijo)
     
     points(rbind(c(fijo,aux)),pch=15,cex=0.4,col="red")
+    
     cat("Iteracion=",i,"\tFunc(x)=",Func(fijo),"\tX=",fijo,"\tError=---\n")
+
     repeat
     {
       fijoN = FuncG(fijo)
@@ -40,14 +41,18 @@ fPuntoFijo = function(a,b)
       fijo = fijoN
       
       aux = Func(fijo)
-      
+      points(rbind(c(i,error)),pch=15,cex=0.4,col="red")
       points(rbind(c(fijo,aux)),pch=15,cex=0.4,col="red")
       
       cat("Iteracion=",i,"\tFunc(x)=",Func(fijo),"\tX=",fijo,"\tError=",error,"\n")
     }
     
   }
-
+  else
+  {
+    cat("En el intervalo[",a,",",b,"] no hay una raiz")
+  }
+  
   
 }
 
