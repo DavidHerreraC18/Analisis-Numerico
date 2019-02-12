e1a4e29dcdb652db4ab7871d899f7b342eae2b19
davidherrera2 <46963875+davidherrera2@users.noreply.github.com>
2019-02-12 14:41:44 -0500

    Update Punto 6.R

--- Talleres/Taller1/Punto 6.R
+++ Talleres/Taller1/Punto 6.R
@@ -1,4 +1,4 @@
-#N�mero a conocer la raiz
+#Número a conocer la raiz
 numero = 5
 
 
@@ -7,29 +7,45 @@ numero = 5
 valorini= 4
 
 Func = function(x) ((x^valorini)-numero)
-gx = function(x) (valorini*x^(valorini-1))
+Dx = function(x) (valorini*x^(valorini-1))
+Hx <- function(x) x - (Func(x)/Dx(x))
 
 newton <- function(a,b) {
-  seqx = seq(0,1,0.1)
-  matplot(seqx,cbind(gx(seqx),Func(seqx)),type="l",col=c("blue","red"))
-  ptoMedio = 1
-  i = 0
-  x = 0.7
-  r<-gx(x)
-  error = 0
-  while(Func(r) != 0 )
+  seqx = seq(a,b,0.1)
+  plot(seqx,Func(seqx),type="l",col="red")
+  if((Func(a)-a)*(Func(b)-b)<0)
   {
-    error<-abs(r-x)
-    if(error >= 1.e-8)
-    {
-      x<-r
+    
+    x<-0.70
+    r<-Hx(x)
+    i<-0
+    
+    while (Func(r) != 0 ) 
+    {    
+      error<-abs(r-x)
+      
+      if(error > 1.e-8)
+      {
+        x<-r
+        
+      }
+      else
+      {
+        break
+      }
+      
+      r<-Hx(x)  
+      i<-i+1
+      points(rbind(c(x,Func(x)),pch=15,cex=0.4,col="green"))
+      cat("I=",i,"\tF(x) =",Func(x),"\tX=",signif(x, digits = 8),"\tE=",error,"\n")
     }
-    r<-gx(x)
-    i = i+1
-    points(rbind(c(x,Func(x))),pch=15,cex=0.4,col="green")
-    cat("Iteracion=",i,"\tFunc(x)=",Func(x),"\tX=",x,"\tError=",error,"\n")
+    
   }
-  cat("Resultado = ", x , " es la raiz ", valorini, "de el numero", numero, "\n")
+  else
+  {
+    cat("No tiene raíz la funcion en ese intervalo.\n")
+  }
+ 
 }
 
-newton(0,1)
+newton(-2,1)
