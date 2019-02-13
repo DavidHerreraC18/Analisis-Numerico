a116b255d3b00955d6f661bf3c15ec663b6eb9f7
davidherrera2 <46963875+davidherrera2@users.noreply.github.com>
2019-02-13 00:59:02 -0500

    Add files via upload

---
+++ Talleres/Taller1/Punto7.py
@@ -0,0 +1,28 @@
+def fx(x):
+#aqui se define la funcion f(x), la propuesta aca es un ejemplo: f(x)=x^2
+	y=x^2
+    
+	return y
+def raiz(a,b,E):
+
+    d=(b+a)/10
+
+    x=a
+
+    y=fx(x)
+
+    while(d>E):
+
+        x= x+d
+
+        yant=y
+
+        y=fx(x)
+
+        if ((y*yant)<0):
+
+            x=x-d
+
+            d=d/10
+
+     return x
